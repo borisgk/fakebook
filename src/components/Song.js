8dc4a6cf38c2d2b54913da000f52b4d25e021a10
Boris Kimel <borisk@rus9n.com>
2023-09-24 16:04:48 +0300

    Canvas version working

--- src/components/Song.js
+++ src/components/Song.js
@@ -5,29 +5,55 @@ export default function Song({songs}) {
 
   const params = useParams()
   const song = songs[params.songID]
+  const chords = song.chords
 
   useEffect(() => {
-    const canvas = document.getElementById('canvas')
-    const ctx = canvas.getContext("2d")
-    ctx.font = "normal 1rem Roboto"
-    let precedingString = ctx.measureText('Дело шло к рассв')
-    let targetSymbol = ctx.measureText('е')
-    let offset = precedingString.width + targetSymbol.width / 2
-    let chordMarker = ctx.measureText('M')
-    let markerLeft = offset - chordMarker.width / 2
+    const canvas = document.getElementById('songcanvas')
+    const ctx = canvas.getContext('2d', {alpha: false})
+    ctx.fillStyle = '#FFFFFF'
+    ctx.fillRect(0, 0, 800, 1500)
+    ctx.fillStyle = '#666666'
+    ctx.font = '18px Roboto'
 
-    const marker = document.getElementById('marker')
-    marker.style.left = `${markerLeft}px`
+    let offsetTop = 54
+    for (let line of song.lyrics) {
+      ctx.fillText(line, 30, offsetTop)
+      offsetTop += 54
+    }
+
+    ctx.fillStyle = '#FF6666'
+
+    // console.log(song.chords)
+    let chordOffsetTop = 36
+    for (let chordLine in chords) {
+      for (let chord in chords[chordLine]) {
+
+        const precedingText = song.lyrics[chordLine].substring(0, chord - 1)
+        console.log(chord, precedingText)
+        const targetText = song.lyrics[chordLine].substring(chord - 1 , chord)
+        console.log(chord, targetText)
+        const chordText = chords[chordLine][chord]
+
+        const precedingTextMetrics = ctx.measureText(precedingText)
+        const targetTextMetrics = ctx.measureText(targetText)
+        const chordTextMetrics = ctx.measureText(chordText)
+
+        const leftOffset = precedingTextMetrics.width + targetTextMetrics.width / 2 - chordTextMetrics.width / 2 + 30
+        
+        ctx.fillStyle = '#FF6666'
+        ctx.fillText(chordText, leftOffset, chordOffsetTop + 54 * chordLine)
+
+        ctx.fillStyle = '#000000'
+        ctx.fillText(targetText, precedingTextMetrics.width + 30, 54 + 54 * chordLine)
+      }
+    }
+
   })
 
   return (
     <div>
       <h2>{song.author} &middot; {song.title}</h2>
-      {song.lyrics.map((line, index) =>
-        <p key={index}>{line}</p>
-      )}
-      <canvas id="canvas"></canvas>
-      <div id='marker'>M</div>
+      <div className='songcanvas'><canvas id="songcanvas" width={800} height={1500}></canvas></div>
     </div>
   )
 }
